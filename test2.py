0ce832694add6d65435c69985be6a3913080f948
boyoungs <101075082+boyoungs@users.noreply.github.com>
2022-03-14 11:55:08 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,15 @@
+# 1 ~ 10 sum
+'''
+sum = 0
+for i in range(2,11,2) :
+    sum += i
+    # a= a + i
+    print(i, "+", sum- i, "sum =", sum)
+print("total =", sum)
+'''
+
+data = 'hello ' +\
+    'pyton ' +\
+    'happy ' +\
+    'coding '
+print(data)
---
+++ test3.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.shape("turtle")
+
+for i in range(0.4) :
+    t.forward(200);
+    t.right(60);
+
+
+turtle.done()
